a9c0c2188bda24086a0ef4a5969e692734b91637
MoonHack2077 <arenasrinconj@gmail.com>
2022-02-04 14:56:25 -0500

    guaaaat

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ import getCharacter from "./fetch.js";
 import Character from "./character.js"
 
 const all = [];
-for(let i = 1; i <=26; i++) {
-    all.push(new Character(await getCharacter(i)));
-}
-all.forEach( x => x.bulid() )
+// for(let i = 1; i <=20; i++) {
+//     all.push(new Character(await getCharacter()));
+// }
+await getCharacter()
+// all.forEach( x => x.bulid() )
