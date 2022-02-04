aea20f239e17acc5ab4f72aecf94694118947ef8
MoonHack2077 <arenasrinconj@gmail.com>
2022-02-03 23:41:33 -0500

    Not was a hotfix xd, but responsive done

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import getCharacter from "./fetch.js";
 import Character from "./character.js"
 
 const all = [];
-for(let i = 1; i <= 27; i++) {
+for(let i = 1; i <=26; i++) {
     all.push(new Character(await getCharacter(i)));
 }
 
