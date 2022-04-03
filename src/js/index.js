2a66bdca294faab5fe30fd654a178f1b94431eb6
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-04-03 00:08:50 -0500

    Init the same project but this time going to be with react, because i want be the best

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import getCharacter from "./fetch.js";
-import Character from "./character.js"
-
-const all = [];
-// for(let i = 1; i <=20; i++) {
-//     all.push(new Character(await getCharacter()));
-// }
-await getCharacter()
-// all.forEach( x => x.bulid() )
