63d880e9fff99041eec1cf17a9b8d9d197d66130
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-04-03 20:37:06 -0500

    functions to do fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import React , { useEffect, useState } from 'react';
 import { NavBar } from './Components/NavBar/NavBar.jsx';
 import { Pagination } from './Components/Pagination/Pagination.jsx';
 import { Characters } from './Components/Characters/Characters.jsx';
 import './App.css';
 
 function App() {
+  const [ characters , setCharacters ] = useState([]);
+  const [ info  , setInfo ] = useState([]);
+
+  const fetchData = url => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+       setCharacters(data.results)
+        setInfo(data.info)  
+    })
+  }
+
+  useEffect( ()=>{
+    fetchData();
+  }, [] )
+
+  const onPrev = () => fetchData(info.prev);
+  const onNext = () => fetchData(info.next);
+  
   return (
     <div className="App">
       <NavBar/>
-      <Pagination/>
-        <Characters/>
-      <Pagination/>
+      <Pagination onPrev={onPrev} onNext={onNext} next={info.next} prev={info.prev}/>
+        <Characters characters={characters} />
+      <Pagination onPrev={onPrev} onNext={onNext} next={info.next} prev={info.prev}/>
     </div>
   );
 }
