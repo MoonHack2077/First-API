617361d16830d0ac0dc06b76d7c66b721a9409aa
JuanManuelArenasRincon20077 <arenasrinconj@gmail.com>
2022-04-08 14:45:34 -0500

    added nice animation, but when the hover has done the transition is too fast

--- src/Components/Character/Character.jsx
+++ src/Components/Character/Character.jsx
@@ -1,21 +1,17 @@
 import React , { Fragment } from 'react';
 import './Character.css';
 
-function Character({ image, name , gender , origin , episode , status }){
+function Character({ image, name }){
     return(
          <Fragment>
-             <li key={name} className="card-character" >
-                <div className="character-img">
-                    <img src={image} alt={name} />
-                </div>
-                <div className="character-info">
-                    <ul>
-                        <li>{name}</li>
-                        <li>{status}</li>
-                        <li>{gender}</li>
-                        <li>{origin.name}</li>
-                        <li>{episode.length}</li>
-                    </ul>
+             <li key={name} className="card-character" style={{
+                 backgroundImage: `url(${image})`
+            }} >
+                <div className='card-content'>
+                    <div className='card-name-character'>
+                        <p>{name}</p>
+                    </div>
+                    <button>Learn More</button>
                 </div>
             </li> 
          </Fragment>
